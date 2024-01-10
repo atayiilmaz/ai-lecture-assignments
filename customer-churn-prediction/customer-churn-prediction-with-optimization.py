71035568e9a09179ab65bc0f6a989f9c30c0e79b
atayiilmaz <yiilmazataberk@gmail.com>
2024-01-10 11:01:35 +0300

    added new machine learning methods and and datasets

--- customer-churn-prediction/customer-churn-prediction-with-optimization.py
+++ customer-churn-prediction/customer-churn-prediction-with-optimization.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 import sys
+from numpy import random
+from random import randint
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -18,22 +20,35 @@ columns.remove("customerID")
 columns.remove("PaymentMethod")
 columns.remove("Contract")
 
+
 # model = KNeighborsClassifier(n_neighbors=3)
 model = GaussianNB()
-
-x = data[columns].values
+min = 0
+max = 0
 y = data[["Churn"]].values
 y = np.ravel(y)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=True)
-model.fit(x_train, y_train)
-predictions = model.predict(x_test)
 
-correct = (y_test == predictions).sum()
-incorrect = (y_test != predictions).sum()
+for i in range(0, 1000):
+    random_chosen_column = random.choice(columns, randint(1, len(columns)))
+    x = data[random_chosen_column].values
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=True)
+    if (len(x_train) != 0 and len(y_train) != 0):
+        model.fit(x_train, y_train)
+        predictions = model.predict(x_test)
 
-acc = accuracy_score(y_test, predictions)
+        correct = (y_test == predictions).sum()
+        incorrect = (y_test != predictions).sum()
+
+        acc = accuracy_score(y_test, predictions)
+        if (i == 0):
+            min = acc
+            max = acc
+        if (acc > max):
+            max = acc
+        if (acc < min):
+            min = acc
 
 print(f"Results for model {type(model).__name__}")
 print(f"Correct: {correct}")
 print(f"Incorrect: {incorrect}")
-print(f"Accuracy: %.2f%%" %(acc * 100))
+print(f"Accuracy: %.2f%%" %(max * 100))
